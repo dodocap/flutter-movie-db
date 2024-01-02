066eb47c748d5daee67ec4fdfbe84fd164948cde
dodocap <dodocap@naver.com>
2024-01-02 16:08:17 +0900

    refactor: 라우터 ChangeNotificationProvider 적용

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,22 +1,30 @@
 import 'package:go_router/go_router.dart';
 import 'package:orm_movie_db/common/constants.dart';
 import 'package:orm_movie_db/ui/movie_detail/movie_detail_screen.dart';
+import 'package:orm_movie_db/ui/movie_detail/movie_detail_view_model.dart';
 import 'package:orm_movie_db/ui/movie_list/movie_list_screen.dart';
+import 'package:orm_movie_db/ui/movie_list/movie_list_view_model.dart';
+import 'package:provider/provider.dart';
 
 final routes = GoRouter(
   initialLocation: '/movie',
   routes: [
     GoRoute(
-      path: '/movie',
-      builder: (_, __) => const MovieListScreen(),
-      routes: [
-        GoRoute(
-          path: 'detail',
-          builder: (_, state) {
-            return MovieDetailScreen(movieId: state.uri.queryParameters[qpMovieId]!);
-          },
-        )
-      ]
-    ),
+        path: '/movie',
+        builder: (_, __) => ChangeNotifierProvider(
+              create: (_) => MovieListViewModel(),
+              child: const MovieListScreen(),
+            ),
+        routes: [
+          GoRoute(
+            path: 'detail',
+            builder: (_, state) {
+              return ChangeNotifierProvider(
+                create: (_) => MovieDetailViewModel(),
+                child: MovieDetailScreen(movieId: state.uri.queryParameters[qpMovieId]!),
+              );
+            },
+          )
+        ]),
   ],
 );
